2ae1844a1a79be7768bf498ab79bd0a75e0d7ede
eescovitchr <90158351+eescovitchr@users.noreply.github.com>
2021-10-09 12:14:28 -0500

    Implementacion de validaciones

--- validaciones.js
+++ validaciones.js
@@ -1,24 +1,114 @@
-function validar_nombreUsuario(string) {
-    const ptr = new RegExp('^[A-Z0-9Ñ]+$', 'i');
-    if (string.length < 3 || string.length > 9) {
-        return false;
-    } else if (!ptr.test(string)) {
-        return false;
-    } else {
-        return true;
-    }
-}
+const form = document.getElementById("form_registro_usuario");
+const submitButton = document.getElementById("btn_enviar_registro");
 
-function validar_contrasena(string) {
-    const ptr = new RegExp('^[A-Z0-9Ñ]+$', 'i');
-    if (string.length < 6) {
-        return false;
-    } else if (!ptr.test(string)) {
-        return false;
-    } else {
-        return true;
-    }
-}
+let timeout = null;
 
-module.exports.validar_nombreUsuario = validar_nombreUsuario;
-module.exports.validar_contrasena = validar_contrasena;
+let errors = {
+  in_usuario: true,
+  correo: true,
+  in_contrasena: true,
+  val_contrasena: true
+};
+
+const mailformatRegex = /^[^@]+@\w+(\.\w+)+\w$/;
+
+document.querySelectorAll('.form-box').forEach((box) => {
+  const boxInput = box.querySelector('input');
+
+  boxInput.addEventListener('keydown', (event) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      console.log(`Input ${boxInput.name} value: `, boxInput.value);
+
+      validation(box, boxInput);
+    }, 300);
+  });
+});
+
+validation = (box, boxInput) => {
+  if (boxInput.value == '') {
+    showError(true, box, boxInput);
+  } 
+  else {
+    showError(false, box, boxInput);
+  }
+  if(boxInput.name == 'in_usuario'){
+    if (boxInput.value.length < 3 || boxInput.value.length >= 9){
+        showError(true, box, boxInput);
+       }
+    else {
+      showError(false, box, boxInput);
+    }
+  }  
+  if(boxInput.name == 'in_usuario'){
+    const ptr = new RegExp('^[A-Z0-9Ñ]+$', 'i');
+    if(!ptr.test(boxInput.value)){
+        console.log("Entro")
+        showError(true, box, boxInput);
+      } 
+  }  
+  if (boxInput.name == 'in_contrasena') {
+    if (boxInput.value.length <= 6) {
+      showError(true, box, boxInput);
+    } 
+    else {
+      showError(false, box, boxInput);
+    }
+  }
+  if (boxInput.name == 'in_contrasena') {
+    const ptr = new RegExp('^[A-Z0-9Ñ]+$', 'i');
+    if(!ptr.test(boxInput.value)){
+        console.log("Entro")
+        showError(true, box, boxInput);
+      } 
+  }
+  if (boxInput.name == 'correo') {
+    if (!boxInput.value.match(mailformatRegex)) {
+      showError(true, box, boxInput);
+    } 
+    else {
+      showError(false, box, boxInput);
+    }
+  }
+  if (boxInput.name == 'val_contrasena') {
+    if (boxInput.value.length <= 6) {
+      showError(true, box, boxInput);
+    } 
+    else {
+      showError(false, box, boxInput);
+    }
+  }
+  submitController();
+};
+
+showError = (check, box, boxInput) => {
+  if (check) {
+    box.classList.remove('form-success');
+    box.classList.add('form-error');
+    errors[boxInput.name] = true;
+  } 
+  else {
+    box.classList.remove('form-error');
+    box.classList.add('form-success');
+    errors[boxInput.name] = false;
+  }
+};
+
+submitController = () => {
+  console.log(errors);
+  if (errors.in_usuario || errors.correo || errors.in_contrasena || errors.val_contrasena) {
+    submitButton.toggleAttribute('disabled', true);
+  } 
+  else {
+    submitButton.toggleAttribute('disabled', false);
+  }
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  console.log([...formData]);
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+});
